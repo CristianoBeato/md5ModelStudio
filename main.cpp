346af3c170acf4182f8fd1ec0312b95502a3fb74
Beato <cristianobeato_dm@hotmail.com>
2025-11-14 22:42:16 -0300

    Begin implement tools dialogs

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include "Gwen/Renderers/OpenGL.h"
 #include "Gwen/Skins/TexturedBase.h"
 #include "Gwen/Controls/WindowCanvas.h"
+#include "ViewPort.hpp"
 
 static const char* k_NOGUI = "--nogui";
 static const char* k_IMPORT = "--import";
@@ -29,7 +30,12 @@ static const char* k_HELP_TEXT =
     "--help         print help\n"
 };
 
-crMain::crMain( void )
+crMain::crMain( void ) : Gwen::Controls::Base( nullptr, "main" ),
+    m_renderer( nullptr ),
+    m_skin( nullptr ),
+    m_canvas( nullptr ),
+    m_menu( nullptr ),
+    m_statusbar( nullptr )
 {
 }
 
@@ -40,13 +46,14 @@ crMain::~crMain( void )
 void crMain::Run(void)
 {
     bool running = true;
-
     if ( !SDL_Init( SDL_INIT_VIDEO | SDL_INIT_EVENTS ) )
         throw std::runtime_error( SDL_GetError() );
 
     CreateUI();
+
     while( !m_canvas->WantsQuit() )
     {
+        // render our ui
         m_canvas->DoThink();
     }
 
@@ -57,19 +64,14 @@ void crMain::Run(void)
 
 void crMain::Open( const std::string &in_file )
 {
-    // we always clear the scene before load a new model
-    Clear();
-
 }
 
 void crMain::Save( const std::string &in_file )
 {
-    // save model to file
 }
 
 void crMain::Import( const std::string &in_file )
 {
-    Clear();
 }
 
 void crMain::Export( const std::string &in_file )
@@ -87,20 +89,78 @@ void crMain::CreateUI(void)
 	// if you try to do it before (The window canvas initializes the renderer)
 	//
 	// Create the skin and renderer
-    Gwen::Renderer::Base* renderer = new Gwen::Renderer::OpenGL();
-    Gwen::Skin::Base* skin = new Gwen::Skin::TexturedBase( renderer );
+    m_renderer = new Gwen::Renderer::OpenGL();
+    m_skin = new Gwen::Skin::TexturedBase( m_renderer );
 
     // The window canvas is a cross between a window and a canvas
 	// It's cool because it takes care of creating an OS specific
 	// window - so we don't have to bother with all that crap.
-    m_canvas = new Gwen::Controls::WindowCanvas( -1, -1, 800, 600, skin, "md5ModelStudio" );
+    m_canvas = new Gwen::Controls::WindowCanvas( -1, -1, 800, 600, m_skin, "md5ModelStudio" );
 
     // Now it's safe to set up the skin
+    dynamic_cast<Gwen::Skin::TexturedBase*>(m_skin)->Init( "./assets/images/gwen_dark.png" );    
+
 #if 1
-    dynamic_cast<Gwen::Skin::TexturedBase*>(skin)->Init( "images/gwen_dark.png" );    
+    dynamic_cast<Gwen::Skin::TexturedBase*>(m_skin)->SetDefaultFont( L"./assets/fonts/Inconsolata.ttf", 14.0f );
 #else
-    dynamic_cast<Gwen::Skin::TexturedBase*>(skin)->Init( "images/DefaultSkin.png" );
+    dynamic_cast<Gwen::Skin::TexturedBase*>(m_skin)->SetDefaultFont( L"./assets/fonts/OpenSans.ttf", 14.0f );
+#endif 
+
+    /// 
+    /// Create Program Layout
+    /// 
+
+    /// menu barr
+    m_menu = new Gwen::Controls::MenuStrip( m_canvas );
+    m_menu->Dock( Gwen::Pos::Top );
+
+    Gwen::Controls::MenuItem* pRoot = m_menu->AddItem( L"File" );
+    pRoot->GetMenu()->AddItem( L"Load", "", "Ctrl+L" )->SetAction( this, &crMain::MenuLoad );
+    pRoot->GetMenu()->AddItem( L"Save", "", "CTRL+S" )->SetAction( this, &crMain::MenuSave );
+    pRoot->GetMenu()->AddItem( L"Import", "", "Ctrl+Shift+S" )->SetAction( this, &crMain::MenuImport );
+    pRoot->GetMenu()->AddItem( L"Export", "", "Ctrl+Shift+S" )->SetAction( this, &crMain::MenuExport );
+    pRoot->GetMenu()->AddItem( L"Quit", "", "Ctrl+Q" )->SetAction( this, &crMain::MenuQui );
+
+    /// Status Barr
+    m_statusbar = new Gwen::Controls::StatusBar( m_canvas );
+	m_statusbar->Dock( Gwen::Pos::Bottom );
+
+    Gwen::Controls::Layout::Table* pCenter = new Gwen::Controls::Layout::Table( m_canvas );
+    pCenter->Dock( Gwen::Pos::Fill );
+
+    Gwen::Controls::Layout::TableRow* pLeft = new Gwen::Controls::Layout::TableRow( pCenter );
+    pLeft->SetBounds( 5, 5, 200, 200 );
+    pLeft->Dock( Gwen::Pos::Left );
+
+    Gwen::Controls::Layout::TableRow* pRight = new Gwen::Controls::Layout::TableRow( pCenter );
+    pRight->Dock( Gwen::Pos::Fill );
+
+    m_dockControlLeft = new Gwen::Controls::TabControl( pLeft );
+	m_jointsTab = m_dockControlLeft->AddPage( L"Joints" );
+	m_meshesTab = m_dockControlLeft->AddPage( L"Meshes" );
+    m_dockControlLeft->Dock( Gwen::Pos::Fill );
+    
+#if 0		
+	Gwen::Controls::Base* pPage = pButton->GetPage();
+	Controls::RadioButtonController* pRadio = new Controls::RadioButtonController( pPage );
+	pRadio->SetBounds( 10, 10, 100, 100 );
+	pRadio->AddOption( "Top" )->Select();
+	pRadio->AddOption( "Bottom" );
+	pRadio->AddOption( "Left" );
+	pRadio->AddOption( "Right" );
+	pRadio->onSelectionChange.Add( m_canvas, &ThisClass::OnDockChange );
 #endif
+
+#if 1
+    m_viewport = new Gwen::Controls::DrawPanel( pRight ); 
+    m_viewport->Dock( Gwen::Pos::Fill );
+#else
+    m_viewport = new crViewPort( pRight );
+    m_viewport->Dock( Gwen::Pos::Fill );
+#endif
+
+    m_statusbar->SetText( "Welcome to MD5Studio" );
+
 }
 
 void crMain::DestroyUI(void)
@@ -110,6 +170,89 @@ void crMain::DestroyUI(void)
         delete m_canvas;
         m_canvas = nullptr;
     }
+
+    if( m_skin )
+    {
+        delete m_skin;
+        m_skin = nullptr;
+    }
+
+    if ( m_renderer )
+    {
+        delete m_renderer;
+        m_renderer = nullptr;
+    }
+}
+
+void crMain::MenuLoad(Gwen::Controls::Base *pControl)
+{
+    if ( m_statusbar )
+    {
+        Gwen::UnicodeString file = Gwen::Utility::StringToUnicode( "fuckk" );
+        m_statusbar->SetText( Gwen::Utility::Format( L"Loading model %s", file.c_str() ) );
+    }
+
+    // we always clear the scene before load a new model
+    Clear();
+
+    if ( m_statusbar )
+    {
+        Gwen::UnicodeString file = Gwen::Utility::StringToUnicode( "fuckk" );
+        m_statusbar->SetText( Gwen::Utility::Format( L"Model %s loaded sucefully", file.c_str() ) );
+    }
+}
+
+void crMain::MenuSave(Gwen::Controls::Base *pControl)
+{
+    if ( m_statusbar )
+    {
+        Gwen::UnicodeString file = Gwen::Utility::StringToUnicode( "fuckk" );
+        m_statusbar->SetText( Gwen::Utility::Format( L"Saving model %s", file.c_str() ) );
+    }
+
+    if ( m_statusbar )
+    {
+        Gwen::UnicodeString file = Gwen::Utility::StringToUnicode( "fuckk" );
+        m_statusbar->SetText( Gwen::Utility::Format( L"Model %s saved sucefully", file.c_str() ) );
+    }
+}
+
+void crMain::MenuExport(Gwen::Controls::Base *pControl)
+{
+    if ( m_statusbar )
+    {
+        Gwen::UnicodeString file = Gwen::Utility::StringToUnicode( "fuck" );
+        m_statusbar->SetText( Gwen::Utility::Format( L"Exporting model %s", file.c_str() ) );
+    }
+
+    if ( m_statusbar )
+    {
+        Gwen::UnicodeString file = Gwen::Utility::StringToUnicode( "fuck" );
+        m_statusbar->SetText( Gwen::Utility::Format( L"Model %s exported sucefully", file.c_str() ) );
+    }    
+
+}
+
+void crMain::MenuImport(Gwen::Controls::Base *pControl)
+{
+    if ( m_statusbar )
+    {
+        Gwen::UnicodeString file = Gwen::Utility::StringToUnicode( "fuckk" );
+        m_statusbar->SetText( Gwen::Utility::Format( L"Importing model %s", file.c_str() ) );
+    }
+
+    Clear();
+
+    if ( m_statusbar )
+    {
+        Gwen::UnicodeString file = Gwen::Utility::StringToUnicode( "fuckk" );
+        m_statusbar->SetText( Gwen::Utility::Format( L"Model %s imported sucefully", file.c_str() ) );
+    }
+}
+
+void crMain::MenuQui(Gwen::Controls::Base *pControl)
+{
+    m_canvas->InputQuit();
 }
 
 int main( int argc, const char* argv[] )
--- main.hpp
+++ main.hpp
@@ -6,9 +6,11 @@
 #ifndef __MAIN_HPP__
 #define __MAIN_HPP__
 
-#include "Gwen/Controls/WindowCanvas.h"
+#include "Gwen/Gwen.h"
+#include "Gwen/Controls.h"
 
-class crMain
+class crViewPort;
+class crMain : public Gwen::Controls::Base
 {    
 public:
     crMain( void );
@@ -22,10 +24,26 @@ public:
     void    Clear( void );
 
 private:
-    Gwen::Controls::WindowCanvas* m_canvas;
+    Gwen::Renderer::Base*           m_renderer;
+    Gwen::Skin::Base*               m_skin;
+    Gwen::Controls::WindowCanvas*   m_canvas;
+    Gwen::Controls::MenuStrip*      m_menu;
+    Gwen::Controls::StatusBar*      m_statusbar;
+    Gwen::Controls::TabControl*     m_dockControlLeft;
+    Gwen::Controls::TabButton*      m_jointsTab;
+    Gwen::Controls::TabButton*      m_meshesTab;
+    Gwen::Controls::DrawPanel*      m_viewport;
 
     void    CreateUI( void );
     void    DestroyUI( void );
+
+    // Events function
+    void MenuLoad( Gwen::Controls::Base* pControl );
+    void MenuSave( Gwen::Controls::Base* pControl );
+    void MenuExport( Gwen::Controls::Base* pControl );
+    void MenuImport( Gwen::Controls::Base* pControl );
+    void MenuQui( Gwen::Controls::Base* pControl );
+
 };
 
 #endif //__MAIN_HPP__
